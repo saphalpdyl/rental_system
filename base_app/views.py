e248661fbb3ec292c0ffdfc1fa0dc4668dc4a655
saphalpdyl <saphalpdyl@gmail.com>
2024-02-08 14:33:58 +0545

    Added is_admin check on vehicle listing request

--- base_app/views.py
+++ base_app/views.py
@@ -192,16 +192,18 @@ class HandleRejectVehicleListingRequest(AuthRequiredMixin, AdminRequiredMixin, V
 
 class HandleAcceptVehicleListingRequest(AuthRequiredMixin, AdminRequiredMixin, View):
     def post(self, request: HttpRequest, request_id: str):
-        vehicle_request = VehicleListingRequests.objects.get(reference_id=request_id)
+        if request.user.is_authenticated:
+            if request.user.is_admin:
+                vehicle_request = VehicleListingRequests.objects.get(reference_id=request_id)
 
-        vehicle = vehicle_request.vehicle
-        vehicle_request.is_reviewed = True
-        vehicle.can_be_listed = True
+                vehicle = vehicle_request.vehicle
+                vehicle_request.is_reviewed = True
+                vehicle.can_be_listed = True
 
-        vehicle.save()
-        vehicle_request.save()
+                vehicle.save()
+                vehicle_request.save()
 
-        return redirect(reverse("admin_vehicle_listing_request_list"))
+                return redirect(reverse("admin_vehicle_listing_request_list"))
 
 
 class VehicleAddForListingView(AuthRequiredMixin, View):
