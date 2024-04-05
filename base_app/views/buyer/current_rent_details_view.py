f94db210a6f0f6540feeeeea5eaf7132a99a8bc6
saphalpdyl <saphalpdyl@gmail.com>
2024-04-05 21:31:25 +0545

    Current rent UI done

---
+++ base_app/views/buyer/current_rent_details_view.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpRequest, HttpResponse
+from django.contrib import messages
+
+from base_app.models import VehicleRent, VehicleRentStatus
+from base_app.mixins import AuthRequiredMixin
+
+
+class CurrentRentDetailsView(AuthRequiredMixin, View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        vehicle_rents = VehicleRent.objects.filter(rent_request__buyer=request.user, status=VehicleRentStatus.ACTIVE)
+
+        return render(request, "base_app/buyer/current_rent_details.html", {
+            "vehicle_rents": vehicle_rents,
+        })
