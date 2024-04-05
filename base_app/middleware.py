139b002ca554c6523d5b8f13dc6fa1105437c8dd
saphalpdyl <saphalpdyl@gmail.com>
2024-04-05 21:41:59 +0545

    Middleware

---
+++ base_app/middleware.py
@@ -0,0 +1,22 @@
+from django.db.models import Q
+from django.utils import timezone
+
+from base_app.models import VehicleRent, VehicleRentStatus
+
+
+class ServiceExpiryCheckMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        # Check every user_service objects in UserService model for active until expiration
+        vehicle_rents = VehicleRent.objects.filter(status=Q(status=VehicleRentStatus.ACTIVE) | Q(VehicleRentStatus.EXTENDED))
+
+        for rent in vehicle_rents:
+            if timezone.now() > rent.expires_at:
+                rent.status = VehicleRentStatus.EXPIRED
+                rent.save()
